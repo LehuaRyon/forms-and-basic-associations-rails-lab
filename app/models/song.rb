a7e2b19bd6250b86bfc12683410a6d2eb41079fa
Jasmine Ryon <jryon12@priorypride.net>
2021-06-15 18:50:27 -0700

    commit tree 7c05f5e3d7b7cbad2b274bbe2cab4dd4dc9638be

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,24 @@
 class Song < ActiveRecord::Base
   # add associations here
+  has_many :notes
+  belongs_to :genre
+  belongs_to :artist
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      self.notes << Note.create(content: content)
+    end
+  end
+  def note_info
+    self.notes.map(&content)
+  end
+  
 end
